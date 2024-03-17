72cd6a7c7280bd6092e6ddc6d4e5a99dbc5f9b5b
Fernando <73785564+fernandopassoss@users.noreply.github.com>
2024-03-17 10:03:58 -0300

    Add files via upload

---
+++ booknook_springboot/src/main/java/com/booknook/booknook_springboot/BooknookSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.booknook.booknook_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknookSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknookSpringbootApplication.class, args);
+	}
+
+}
---
+++ booknook_springboot/src/test/java/com/booknook/booknook_springboot/BooknookSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booknook.booknook_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknookSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
